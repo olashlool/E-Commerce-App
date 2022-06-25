694cc564f7c1a41784e293af459eac0389130ab1
Ola M AL-Shlool <olashlool94@gmail.com>
2022-06-25 16:02:31 +0300

    Final-ECommerce

--- Cosmetic-Store/Cosmetic-Store/Models/Services/EmailServices.cs
+++ Cosmetic-Store/Cosmetic-Store/Models/Services/EmailServices.cs
@@ -7,6 +7,10 @@ namespace Cosmetic_Store.Models.Services
 {
     public class EmailServices : IEmail
     {
+        /* 1. Brings in string type of email, subject, and message contents
+           2. Brings in the sender from configuration and create a new SendGridMessage
+           3. Set the message properties to have a sender, an email subject, and email contents */
+
         public async Task SendEmailAsync(string email, string subject, string message)
         {
             SendGridClient client = new SendGridClient("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");
